d109a403208c68a193799ffba94231d248f4037d
Dannyb <dannyb4me@cbn.gov>
2017-06-05 16:07:51 +0100

    fix header issues on server.js

--- server/server.js
+++ server/server.js
@@ -21,25 +21,6 @@ const express = require('express'),
 }
 
 app.use(allowCrossDomain);
-// // Add headers
-// app.use(function (req, res, next) {
-
-//     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-
-//     // Pass to next layer of middleware
-//     next();
-// });
 
 // for parsing application/x-www-form-urlencoded)
 app.use(bodyParser.urlencoded({ extended: true }));
