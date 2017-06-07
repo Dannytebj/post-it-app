022a5dc976e6d4d70915f1ac9170ed407b951d65
Dannyb <dannyb4me@cbn.gov>
2017-06-07 10:39:55 +0100

    Add welcome Landing Page

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import Login from './js/components/login';
+import Welcome from './js/components/main/main';
 
 ReactDOM.render(
   <Router history={ browserHistory }>
       <Route path="login" component={ Login } />
+      <Route path= "main" component={ Welcome } />
   </Router>,
   document.getElementById('main-container')
 );
