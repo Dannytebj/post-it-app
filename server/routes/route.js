1da7fbdd014cf4001aba73c6b56617ee9127fbaa
Dannyb <dannyb4me@cbn.gov>
2017-07-02 17:51:19 +0100

    refactor add user route/api again

--- server/routes/route.js
+++ server/routes/route.js
@@ -130,7 +130,7 @@ router.post('/group/:groupId/users', (req, res) => {
     currUser = auth.currentUser,
     userId = req.body.userId;
   if (currUser) {
-    const promise = db.ref('group/' + groupId + '/users').update(
+    const promise = db.ref('group/' + groupId + '/users').push(
       { UserId: userId, }
     );
     promise.then(() => {
