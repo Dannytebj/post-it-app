300db6dfeb570724de7abd5a55c783aeb20f5bae
Dannyb <dannyb4me@cbn.gov>
2017-07-06 14:33:18 +0100

    Refactor create group route

--- client/src/js/components/pages/groups/groupList/index.js
+++ client/src/js/components/pages/groups/groupList/index.js
@@ -5,19 +5,15 @@ import Group from './group.js';
 const GroupList = ({ groupList }) => {
     return (<ul id="myDiv">
         {
-            Object.keys(groupList).map((key, index) =>{
-                console.log(groupList[index]);
-                return (<Group group ={groupList[key]} key={index} />)
+            groupList.map((group, index) => {
+                return (<Group group={group} key={index}/>);
             })
-            /*groupList.map((group, index) => {
-                return (<Group group={groups} key={index}/>);
-            })*/
         }
     </ul>);
 };
 
 GroupList.propTypes = {
-    groupList: PropTypes.object.isRequired
+    groupList: PropTypes.array.isRequired
 };
 
 export default GroupList;
