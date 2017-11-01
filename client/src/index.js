8d4ea4c8f75aa572260f4e5d8642886ba7357313
Daniel Atebije <dannytebj@gmail.com>
2017-11-01 16:11:08 +0100

    Revert "Ft client tests"

--- client/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Routes from '../src/js/components/Routes';
-import './scss/index.scss';
-
-
-ReactDOM.render(<Routes/>,
-  document.getElementById('app'));
